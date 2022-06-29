307c99a8523737c683e4f61845d7eac6f6fc4f7f
Gu-Heping <yue76093219@126.com>
2022-06-29 14:08:09 +0800

    Site updated: 2022-06-29 14:08:09

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        // $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png"); //图标更改
         document.title = '点我！( •̀ ω •́ )✧';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        // $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png"); //图标更改
         document.title = '嘻嘻q(≧▽≦q)' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
